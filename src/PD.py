65713e2830a3eac7c6a42389bc421f991b558311
Simon Morais Da Cunha <smoraisd@ulb.ac.be>
2021-03-15 08:33:50 +0100

    Working DQN + Trained Model

--- src/PD.py
+++ src/PD.py
@@ -1,24 +1,23 @@
 from __future__ import annotations
 import numpy as np
-from collections import Counter
 import itertools as it
-import typing
 import random
+
+from numpy.lib.npyio import load
 from tqdm import tqdm
 
-#All RL packafe
+# All RL packafe
 import torch as T
 import torch.nn as nn
-import torch.nn.functional as F #for the activation function
+import torch.nn.functional as F  # for the activation function
 import torch.optim as optim
 
-from numpy.core.arrayprint import _leading_trailing
-from numpy.core.numeric import NaN
-
-Strategy = {'C':0 , 'D':1}
+Strategy = {'C': 0, 'D': 1}
 PD = [[3, 0], [5, 1]]
+MODEL_NAME = "DQN_PD_TIT_FOR_TAT"
 
-class DeepQNetwork(nn.Module): #Give access to a lot of function
+
+class DeepQNetwork(nn.Module):  # Give access to a lot of function
 
     def __init__(self, lr, input_dim, fc1_dim, fc2_dim, output_dim):
         super(DeepQNetwork, self).__init__()
@@ -27,10 +26,12 @@ class DeepQNetwork(nn.Module): #Give access to a lot of function
         self.fc2_dim = fc2_dim
         self.output_dim = output_dim
 
+        # The NN contains 3 layer: one input, one output, and one hidden
         self.fc1 = nn.Linear(*self.input_dim, self.fc1_dim)
         self.fc2 = nn.Linear(self.fc1_dim, self.fc2_dim)
         self.fc3 = nn.Linear(self.fc2_dim, self.output_dim)
 
+        # Optimize and loss function for back propagation and gradient descent
         self.optimize = optim.Adam(self.parameters(), lr=lr)
         self.loss = nn.MSELoss()
 
@@ -38,15 +39,18 @@ class DeepQNetwork(nn.Module): #Give access to a lot of function
         self.to(self.device)    
 
     def forward(self, state):  
+        # Apply the function one after the other
         x = F.relu(self.fc1(state))
         x = F.relu(self.fc2(x))
         action = self.fc3(x)
-        return action    
+        return action
+
 
 class DeepRLNetwork():
 
-    def __init__(self, gamma, epsilon, lr, input_dim, batch_size, n_actions, max_mem_size=100000,
-        eps_end=0.01, eps_dec=5e-4):
+    def __init__(self, gamma, epsilon, lr, input_dim, batch_size, n_actions,
+                max_mem_size=100000, eps_end=0.01, eps_dec=5e-4):
+                
         self.gamma = gamma
         self.epsilon = epsilon
         self.eps_min = eps_end
@@ -55,7 +59,8 @@ class DeepRLNetwork():
         self.actions_space = [i for i in range(n_actions)]
         self.mem_size = max_mem_size
         self.batch_size = batch_size
-        self.mem_cntr = 0
+        self.mem_cntr = 0  # For the next place in our memory, could use deque 
+                           # Numpy Array faster
 
         self.Q_eval = DeepQNetwork(lr, input_dim=input_dim, fc1_dim=256, fc2_dim=256, output_dim=n_actions)
         
@@ -69,6 +74,7 @@ class DeepRLNetwork():
     def storeTransition(self, state, action, reward, new_state):
         index = self.mem_cntr%self.mem_size
 
+        # Store all the information in our memory
         self.state_memory[index] = state
         self.new_state_memory[index] = new_state
         self.reward_memory[index] = reward
@@ -78,11 +84,19 @@ class DeepRLNetwork():
 
     def chooseAction(self, observation):
         if np.random.rand() > self.epsilon:
+            # PyTorch needs us to specify the type (here nn.Linear only works with float) 
+            # and then send it to the device 
             state = T.tensor([observation], dtype=T.float).to(self.Q_eval.device)
             actions = self.Q_eval.forward(state)
             action = T.argmax(actions).item() #Because return a tensor
         else:
             action = np.random.choice(self.actions_space)
+        return action
+
+    def testState(self, observation):
+        state = T.tensor([observation], dtype=T.float).to(self.Q_eval.device)
+        actions = self.Q_eval.forward(state)
+        action = T.argmax(actions).item()
         return action
 
     def learn(self):
@@ -91,16 +105,29 @@ class DeepRLNetwork():
 
         self.Q_eval.optimize.zero_grad() #Particular in pytorch
         max_mem = min(self.mem_size, self.mem_cntr) #until self.mem_size < self.mem_cntr
+
+        # we select a cetain amount of element in our memory
+        # max_mem = the maximum index we can get(either the counter or the memory size)
+        # batch_size = number of element we want
         batch = np.random.choice(max_mem, self.batch_size, replace=False)
         
+        # Array of indices, necessary for retrieving value of batch
         batch_index = np.arange(self.batch_size, dtype=np.int32)
 
+        # We get all the necessary
         state_batch = T.tensor(self.state_memory[batch]).to(self.Q_eval.device)
         new_state_batch= T.tensor(self.new_state_memory[batch]).to(self.Q_eval.device)
         reward_batch = T.tensor(self.reward_memory[batch]).to(self.Q_eval.device)
         action_batch = self.action_memory[batch]
-
+        
+        # [batch_index, action_batch] -> for every line in our batch (batch_index=0,1,2,3....)
+        # retrieve the action we have taken (no meaning taking the value of the action we haven't)
+        # taken. /!\ Using [:, action_batch] don't achieve the same because for the first line
+        # it's gonna do ALL the action_batch and then repeat for line 2, 3.... 
+        # -> batch_size x batch_size matrix !
         q_eval = self.Q_eval.forward(state_batch)[batch_index, action_batch]
+
+        # Here, we want both in order to do the max for the q-learing
         q_next = self.Q_eval.forward(new_state_batch)
 
         q_target = reward_batch + self.gamma * T.max(q_next, dim=1)[0]
@@ -210,43 +237,50 @@ class TitForTatAgent(Agent):
         self.action = opponent_action
     
 
+def saveTrainedModel(model, name):
+    T.save(model.state_dict(), f"../Models/{name}.pt")
 
-if __name__ == "__main__":
+def loadTrainedModel(name):
+    model = DeepQNetwork(fc1_dim=256, fc2_dim=256, lr=0.01, input_dim=[2], output_dim=2)
+    model.load_state_dict(T.load(f"../Models/{name}.pt"))
+    model.eval()
+    return model
 
-    rl_agent =DeepRLNetwork(gamma=0.8, epsilon=0.1, lr=0.01, input_dim=[2], batch_size=64, n_actions=2)
-    adversary  = DefectiveAgent()
-
+def train():
+    rl_agent = DeepRLNetwork(gamma=0.8, epsilon=0.1, lr=0.01, input_dim=[2], batch_size=64, n_actions=2)
+    adversary  = TitForTatAgent()
     nbr_episode = 100000
-
     state = np.array([0,0])
     new_state = np.zeros(2)
-
     cooperation_level = 0
-
-    print(rl_agent.Q_eval.forward(T.tensor([0, 0], dtype=T.float)))
-
     for i in tqdm(range(nbr_episode)):
-
         rl_action = rl_agent.chooseAction(state)
         adversary_action = adversary.getAction()
-
         new_state[0], new_state[1] = rl_action, adversary_action
         reward = PD[rl_action][adversary_action]
-
         rl_agent.storeTransition(state, rl_action, reward, new_state)
         rl_agent.learn()
-
         state = new_state
         adversary.update(rl_action)
         cooperation_level += rl_action
-
+    saveTrainedModel(rl_agent.Q_eval, MODEL_NAME)
     print(1-(cooperation_level/nbr_episode))
-    print(rl_agent.Q_eval.forward([0, 0]))
-    print(rl_agent.Q_eval.forward([0, 1]))
-    print(rl_agent.Q_eval.forward([1, 0]))
-    print(rl_agent.Q_eval.forward([1, 1]))
+
+def test():
+    rl_agent = DeepRLNetwork(gamma=0.8, epsilon=0.1, lr=0.01, input_dim=[2], batch_size=64, n_actions=2)
+    rl_agent.Q_eval = loadTrainedModel(MODEL_NAME)
+
+    print("0 - 0:", rl_agent.testState([0,0]))
+    print("0 - 1:", rl_agent.testState([0,1]))
+    print("1 - 0:", rl_agent.testState([1,0]))
+    print("1 - 1:", rl_agent.testState([1,1]))
+
+if __name__ == "__main__":
+    train()
+    # test()
+    
+    
 
 
-    print(rl_agent)
 
 
